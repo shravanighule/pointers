0d4b3d8eabaa8563a5bb5122c0285e7b5bdf80ae
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:44:06 +0530

    Add files via upload

---
+++ increment pointer.cpp
@@ -0,0 +1,17 @@
+//shravanighule
+//23070123056
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int*aptr=&a;
+    cout<<aptr<<endl;
+    
+    aptr++;
+    cout<<aptr<<endl;
+
+    return 0;
+}
+
+
